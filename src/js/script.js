d22316768118deae94efceaa70bcf8dbb6c58ff0
Gustavo <Gustavohiruo@gmail.com>
2025-11-01 03:06:54 -0300

    Criando o slide show e mudando a cor do botão

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,28 @@
 function trocar(cor){
   document.body.style.background=cor;  
 }
+
+let imagens=[
+  "src/assets/imagemSlideShow1.jpg",
+  "src/assets/imagemSlideShow2.jpg",
+  "src/assets/imagemSlideShow3.jpg",
+  "src/assets/imagemSlideShow4.jpg",
+  "src/assets/imagemSlideShow5.jpg",
+];
+
+//DECLARANDO AS VARIAVEIS
+
+let i=0;
+let tempo =5000; // milisegundos
+
+//função 
+
+function slideShow(){
+    document.getElementById("image").src=imagens[i];
+    i++;
+    if(i == imagens.length){
+        i=0;
+    }
+    setTimeout(slideShow,tempo)
+}
+slideShow();
