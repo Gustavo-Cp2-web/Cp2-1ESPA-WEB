e68f634fe2fa7a2aae064813c05ae0f63719af14
Gustavo <Gustavohiruo@gmail.com>
2025-11-01 17:52:59 -0300

    Terminando a pagina portifolio.html e agicionando as imagens

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,1 +1,33 @@
 //Script do Quiz
+lert("Quiz sobre jogos")
+
+
+const perguntas= [
+ {
+    pergunta:"",
+    resposta:""
+ },
+ {
+    pergunta:"",
+    resposta:""
+ },
+ {
+    pergunta:"",
+    resposta:""
+ },
+
+];
+
+let acertos = 0;
+
+
+for(let i=0; i<perguntas.length;i++){
+    const respostaUsuario=prompt(perguntas[i].pergunta);
+
+    if(respostaUsuario.toLowerCase() === perguntas[i].resposta.toLocaleLowerCase()){
+        acertos++;
+    }
+}
+document.getElementById("msg").innerHTML=(`Você aceretou ${acertos} de ${perguntas.length} perguntas`)
+
+
